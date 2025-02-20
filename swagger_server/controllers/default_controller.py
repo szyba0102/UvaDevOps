cebe2562dc1799e60037575a447fe2d1e9d54a40
Weronika Szybińska <wszybinska@student.agh.edu.pl>
2025-02-20 22:24:41 +0100

    sec commit

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -3,21 +3,34 @@ import six
 
 from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
+from swagger_server.service.student_service import *
 
 
-def add_student(body=None):  # noqa: E501
+def add_student(body=None): # noqa: E501
     """Add a new student
-
     Adds an item to the system # noqa: E501
-
     :param body: Student item to add
     :type body: dict | bytes
-
-    :rtype: str
+    :rtype: float
     """
     if connexion.request.is_json:
-        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        body = Student.from_dict(connexion.request.get_json()) # noqa: E501
+    return add(body)
+    return 500,'error'
+
+# def add_student(body=None):  # noqa: E501
+#     """Add a new student
+#
+#     Adds an item to the system # noqa: E501
+#
+#     :param body: Student item to add
+#     :type body: dict | bytes
+#
+#     :rtype: str
+#     """
+#     if connexion.request.is_json:
+#         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
+#     return 'do some magic!'
 
 
 def delete_student(student_id):  # noqa: E501
@@ -30,7 +43,8 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: object
     """
-    return 'do some magic!'
+    return delete(student_id)
+    return 500,'error'
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +57,5 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    return get_by_id(student_id)
+    return 500,'error'
